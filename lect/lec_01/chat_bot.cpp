2d14d1ad567102d359054df08c4a86e7ec8c55a1
davidkoo1 <sverceks@gmail.com>
2021-01-07 14:28:51 +0200

    Cpp program

---
+++ lect/lec_01/chat_bot.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main(){
+	using namespace std;
+	cout<<"Hello, Wath is yout name?!" <<endl <<">";
+	string name;
+	cin>>name;
+	cout<<"Hello " <<name <<" nice too mit you" <<endl;
+	int age;
+	cout<<"How your age?" <<endl <<">";
+	cin>>age;
+	cout<<"Wow, I believe you have "<<age+1<<endl;
+
+	return 0;
+
+}	
